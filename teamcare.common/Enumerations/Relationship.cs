37392058199a0b2c6a81f21ea61e02a2d29f8c4d
nishidhkagathara <nish.kagathara0791@gmail.com>
2021-08-25 11:37:10 +0530

    Relationship.cs

---
+++ teamcare.common/Enumerations/Relationship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace teamcare.common.Enumerations
+{
+	public enum Relationship
+	{
+		[Description("Sibling")]
+		Sibling = 1,
+		[Description("Spouse")]
+		Spouse = 2,
+		[Description("Parent")]
+		Parent = 3,
+		[Description("Child")]
+		Child = 4,
+		[Description("Grand Parent")]
+		GP = 5,
+		[Description("Other")]
+		Other = 6,
+	}
+}
