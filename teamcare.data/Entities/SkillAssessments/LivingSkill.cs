2df888553eadf1317ef3ce508b459a862b11eedb
nishidhkagathara <nish.kagathara0791@gmail.com>
2021-09-10 14:20:35 +0530

    Entities/SkillAssessments/LivingSkill.cs

---
+++ teamcare.data/Entities/SkillAssessments/LivingSkill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace teamcare.data.Entities.SkillAssessments
+{
+	[Table("LivingSkills")]
+	public class LivingSkill:BaseEntity
+	{
+		[ForeignKey("SkillGroup")]
+		[Column("group_id")]
+		public Guid GroupId { get; set; }
+		public virtual SkillGroup SkillGroup { get; set; }
+
+		[Column("skill_name")]
+		[Required]
+		public string SkillName { get; set; }
+
+		[Column("skill_position")]
+		public int Position { get; set; } = 0;
+	}
+}
---
+++ teamcare.data/Entities/SkillAssessments/SkillGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace teamcare.data.Entities.SkillAssessments
+{
+	[Table("SkillGroups")]
+	public class SkillGroup : BaseEntity
+	{
+		[Column("group_name")]
+		[Required]
+		public string GroupName { get; set; }
+
+		[Column("group_position")]
+		public int Position { get; set; } = 0;
+	}
+}
