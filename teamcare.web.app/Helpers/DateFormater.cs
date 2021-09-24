4e653736d6c22b340a949a9dae9ef63756d0a480
nishidhkagathara <nish.kagathara0791@gmail.com>
2021-09-24 19:07:10 +0530

    Date format changer added

---
+++ teamcare.web.app/Helpers/DateFormater.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace teamcare.web.app.Helpers
+{
+	public static class DateFormater
+	{
+        public static string GetDateCurrentFormat(this DateTimeOffset dt, bool isWithTime)
+        {
+			if (isWithTime)
+			{
+                return dt.ToString("dd MMM yyyy mm:ss tt");
+			}
+			else
+			{
+				return dt.ToString("dd MMM yyyy");
+			}
+        }
+    }
+}
