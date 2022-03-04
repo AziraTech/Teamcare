18c29a873747e77ec07163e31b6f60086e55acf6
nishidhkagathara <nish.kagathara0791@gmail.com>
2022-03-04 19:11:07 +0530

    Common ServiceUserLogCategory.cs

---
+++ teamcare.common/Enumerations/ServiceUserLogCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace teamcare.common.Enumerations
+{
+	public enum ServiceUserLogCategory
+	{
+		[Description("Medical/Appointments")]
+		MedicalAppointments = 1,
+		[Description("Incident/Accident")]
+		IncidentAccident = 2,
+		[Description("Observation")]
+		Observation = 3
+	}
+}
